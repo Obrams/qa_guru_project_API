4060a658d6a7c277a72356dd19f7c044e193b283
NOzerov <testirochik@gmail.com>
2023-10-10 09:17:51 +0300

    fix tests

--- tests/test_get_users.py
+++ tests/test_get_users.py
@@ -2,11 +2,113 @@ import requests
 import allure
 from jsonschema.validators import validate
 
-from conftest import open_json_schema
+from conftest import open_json_schema, request_api
 
 from allure_commons.types import Severity
 
-base_url = 'https://reqres.in'
+service = 'reqres'
+
+
+@allure.feature('Ресурс "Reqres.in"')
+@allure.label('owner', 'Nikita')
+@allure.tag('api')
+@allure.severity(Severity.CRITICAL)
+@allure.title('Создание пользователя')
+def test_create_user():
+    response = request_api(
+        service,
+        'post',
+        url='/api/users',
+        data={'name': 'quagurov', 'job': 'student'}
+    )
+    assert response.status_code == requests.codes.created
+    assert response.json()['name'] == 'quagurov'
+    assert response.json()['job'] == 'student'
+
+
+@allure.feature('Ресурс "Reqres.in"')
+@allure.label('owner', 'Nikita')
+@allure.tag('api')
+@allure.severity(Severity.CRITICAL)
+@allure.title('Обновление данных пользователя')
+def test_update_user():
+    response = request_api(
+        service,
+        'put',
+        url='/api/users/2',
+        data={'name': 'quagurov', 'job': 'best student'}
+    )
+
+    assert response.status_code == 200
+    assert response.json()['name'] == 'quagurov'
+    assert response.json()['job'] == 'best student'
+
+
+@allure.feature('Ресурс "Reqres.in"')
+@allure.label('owner', 'Nikita')
+@allure.tag('api')
+@allure.severity(Severity.CRITICAL)
+@allure.title('Успешная регистрация нового пользователя')
+def test_register_successful_user():
+    response = request_api(
+        service,
+        'post',
+        url='/api/register',
+        data={'email': 'eve.holt@reqres.in', 'password': 'pistol'}
+    )
+
+    assert response.status_code == 200
+    assert response.json()['id'] == 4
+    assert response.json()['token'] == 'QpwL5tke4Pnpja7X4'
+
+
+@allure.feature('Ресурс "Reqres.in"')
+@allure.label('owner', 'Nikita')
+@allure.tag('api')
+@allure.severity(Severity.CRITICAL)
+@allure.title('Успешная авторизация')
+def test_login_successful_user():
+    response = request_api(
+        service,
+        'post',
+        url='/api/login',
+        data={'email': 'eve.holt@reqres.in', 'password': 'cityslicka'}
+    )
+
+    assert response.status_code == 200
+    assert response.json()['token'] == 'QpwL5tke4Pnpja7X4'
+
+
+@allure.feature('Ресурс "Reqres.in"')
+@allure.label('owner', 'Nikita')
+@allure.tag('api')
+@allure.severity(Severity.CRITICAL)
+@allure.title('Авторизация под невалидными данными ')
+def test_login_unsuccessful_user():
+    response = request_api(
+        service,
+        'post',
+        url='/api/login',
+        data={'email': 'peter@klaven'}
+    )
+
+    assert response.status_code == 400
+    assert response.json()['error'] == 'Missing password'
+
+
+@allure.feature('Ресурс "Reqres.in"')
+@allure.label('owner', 'Nikita')
+@allure.tag('api')
+@allure.severity(Severity.CRITICAL)
+@allure.title('Валидация схемы users')
+def test_users_schema():
+    schema = open_json_schema('get_users_schema.json')
+    response = request_api(
+        service,
+        'get',
+        url='/api/users'
+    )
+    validate(instance=response.json(), schema=schema)
 
 
 @allure.feature('Ресурс "Reqres.in"')
@@ -16,10 +118,14 @@ base_url = 'https://reqres.in'
 @allure.title('Получение информации о странице с пользователями')
 def test_users_page_info():
     per_page = 6
-    response = requests.get(
-        url=f'{base_url}/api/users',
+
+    response = request_api(
+        service,
+        'get',
+        url='/api/users',
         params={'per_page': per_page}
     )
+
     assert response.status_code == 200
     assert response.json()['page'] == 1
     assert response.json()['per_page'] == per_page
@@ -32,9 +138,12 @@ def test_users_page_info():
 @allure.severity(Severity.NORMAL)
 @allure.title('Получение информации о одном пользователе')
 def test_check_single_user():
-    response = requests.get(
-        url=f'{base_url}/api/users/2'
+    response = request_api(
+        service,
+        'get',
+        url='/api/users/2'
     )
+
     assert response.status_code == 200
     assert response.json()['data']['email'] == 'janet.weaver@reqres.in'
     assert response.json()['data']['first_name'] == 'Janet'
@@ -50,41 +159,13 @@ def test_check_single_user():
 @allure.severity(Severity.NORMAL)
 @allure.title('Получение информации о пользователе, которого нет в системе.')
 def test_users_not_found():
-    response = requests.get(
-        url=f'{base_url}/api',
-        params={'users': 23}
+    response = request_api(
+        service,
+        'get',
+        url='/api/users/23',
     )
+
     assert response.status_code == 404
-
-
-@allure.feature('Ресурс "Reqres.in"')
-@allure.label('owner', 'Nikita')
-@allure.tag('api')
-@allure.severity(Severity.CRITICAL)
-@allure.title('Создание пользователя')
-def test_create_user():
-    response = requests.post(
-        url=f'{base_url}/api/users',
-        data={'name': 'quagurov', 'job': 'student'}
-    )
-    assert response.status_code == requests.codes.created
-    assert response.json()['name'] == 'quagurov'
-    assert response.json()['job'] == 'student'
-
-
-@allure.feature('Ресурс "Reqres.in"')
-@allure.label('owner', 'Nikita')
-@allure.tag('api')
-@allure.severity(Severity.CRITICAL)
-@allure.title('Обновление данных пользователя')
-def test_update_user():
-    response = requests.put(
-        url=f'{base_url}/api/users/2',
-        data={'name': 'quagurov', 'job': 'best student'}
-    )
-    assert response.status_code == 200
-    assert response.json()['name'] == 'quagurov'
-    assert response.json()['job'] == 'best student'
 
 
 @allure.feature('Ресурс "Reqres.in"')
@@ -93,25 +174,13 @@ def test_update_user():
 @allure.severity(Severity.NORMAL)
 @allure.title('Удаление данных о пользователе из системы')
 def test_delete_user():
-    response = requests.delete(
-        url=f'{base_url}/api/users/2',
+    response = request_api(
+        service,
+        'delete',
+        url='/api/users/2'
     )
+
     assert response.status_code == 204
-
-
-@allure.feature('Ресурс "Reqres.in"')
-@allure.label('owner', 'Nikita')
-@allure.tag('api')
-@allure.severity(Severity.CRITICAL)
-@allure.title('Успешная регистрация нового пользователя')
-def test_register_successful_user():
-    response = requests.post(
-        url=f'{base_url}/api/register',
-        data={'email': 'eve.holt@reqres.in', 'password': 'pistol'}
-    )
-    assert response.status_code == 200
-    assert response.json()['id'] == 4
-    assert response.json()['token'] == 'QpwL5tke4Pnpja7X4'
 
 
 @allure.feature('Ресурс "Reqres.in"')
@@ -120,50 +189,12 @@ def test_register_successful_user():
 @allure.severity(Severity.NORMAL)
 @allure.title('Регистрация под невалидными данными')
 def test_register_unsuccessful_user():
-    response = requests.post(
-        url=f'{base_url}/api/register',
+    response = request_api(
+        service,
+        'post',
+        url='/api/register',
         data={'email': 'sydney@fife'}
     )
+
     assert response.status_code == 400
     assert response.json()['error'] == 'Missing password'
-
-
-@allure.feature('Ресурс "Reqres.in"')
-@allure.label('owner', 'Nikita')
-@allure.tag('api')
-@allure.severity(Severity.CRITICAL)
-@allure.title('Успешная авторизация')
-def test_login_successful_user():
-    response = requests.post(
-        url=f'{base_url}/api/login',
-        data={'email': 'eve.holt@reqres.in', 'password': 'cityslicka'}
-    )
-    assert response.status_code == 200
-    assert response.json()['token'] == 'QpwL5tke4Pnpja7X4'
-
-
-@allure.feature('Ресурс "Reqres.in"')
-@allure.label('owner', 'Nikita')
-@allure.tag('api')
-@allure.severity(Severity.CRITICAL)
-@allure.title('Авторизация под невалидными данными ')
-def test_login_unsuccessful_user():
-    response = requests.post(
-        url=f'{base_url}/api/login',
-        data={'email': 'peter@klaven'}
-    )
-    assert response.status_code == 400
-    assert response.json()['error'] == 'Missing password'
-
-
-@allure.feature('Ресурс "Reqres.in"')
-@allure.label('owner', 'Nikita')
-@allure.tag('api')
-@allure.severity(Severity.CRITICAL)
-@allure.title('Валидация схемы users')
-def test_users_schema():
-    schema = open_json_schema('get_users_schema.json')
-    response = requests.get(
-        url=f'{base_url}/api/users'
-    )
-    validate(instance=response.json(), schema=schema)
